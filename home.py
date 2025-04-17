380878904e1c330c55b66cb365208bad4d1e791b
DonZa47 <nudbun73@gmail.com>
2025-04-17 15:27:03 +0700

    Update home.py

--- home.py
+++ home.py
@@ -85,7 +85,7 @@ if st.button("ทำนายผล"):
    y = dt.Stage  
 
    Knn_model = KNeighborsClassifier(n_neighbors=3)
-   Knn_model.fit(X, y)  
+   Knn_model.fit(X,y)  
     
    x_input = np.array([[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16,A17,A18]])
    st.write(Knn_model.predict(x_input))
